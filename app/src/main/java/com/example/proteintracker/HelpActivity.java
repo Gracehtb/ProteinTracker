9a910bb70412d6f78c6c5fd5e5b9ec5c8072aaf4
vlrntnsha13 <valeriana.tanesha@si.ukdw.ac.id>
2019-09-02 18:51:59 +0700

    Tugas2

---
+++ app/src/main/java/com/example/proteintracker/HelpActivity.java
@@ -0,0 +1,32 @@
+package com.example.proteintracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+public class HelpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        TextView myTextView = new TextView(this);
+        myTextView.setText("Some Help Text");
+        setContentView(myTextView);
+
+        if (savedInstanceState != null) {
+            Log.d("ProteinTracker", savedInstanceState.getString("abc"));
+
+        }
+        Bundle b = getIntent().getExtras();
+        String helpText = b.getString("helpString");
+        myTextView.setText(helpText);
+    }
+}
